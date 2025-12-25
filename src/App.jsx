531e02422262509176e0e6177cab4dcf449c3430
gajju44 <gajjunaphade@gmail.com>
2025-12-25 13:08:41 +0530

    build issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Page1 from './pages/page1/page1.jsx';
+import Page1 from './pages/page1/Page1.jsx';
 import Home from './pages/Home.jsx';
 
 function App() {
